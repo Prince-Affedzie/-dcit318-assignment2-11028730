f60e1a7d9e6f5780b2be94d88f26da106fe3f607
Prince-Affedzie <paffedzie001@st.ug.edu.gh>
2025-08-08 15:50:17 +0000

    Implemented Abstract Classes And Methods

---
+++ Abstract_Classes_And_Methods/Abstract_Classes_And_Methods/Program.cs
@@ -0,0 +1,16 @@
+﻿using Abstract_Classes_And_Methods;
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Circle myCircle = new Circle(10);
+        Console.WriteLine($"The area of the circle is: {myCircle.GetArea()}");
+
+        
+        Rectangle myRectangle = new Rectangle(5, 8);
+        Console.WriteLine($"The area of the rectangle is: {myRectangle.GetArea()}");
+    }
+}
