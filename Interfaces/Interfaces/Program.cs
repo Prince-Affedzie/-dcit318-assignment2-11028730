914ea7629ef969d8460d28925e0873c10e5daf3b
Prince-Affedzie <paffedzie001@st.ug.edu.gh>
2025-08-08 16:49:10 +0000

    Implemented Interface

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Interfaces;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Car myCar = new Car();
+        myCar.Move(); 
+
+        
+        Bicycle myBicycle = new Bicycle();
+        myBicycle.Move();
+    }
+}
